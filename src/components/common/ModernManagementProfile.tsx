68fafb930ebef3428867472d506eaec899605a14
pavanm2216 <pavankumarreddym764@gmail.com>
2026-01-27 22:29:50 +0530

    first commit

--- src/components/common/ModernManagementProfile.tsx
+++ src/components/common/ModernManagementProfile.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/Avatar';
 import { Badge } from '../ui/badge';
-import { Card, CardContent } from '../ui/Card';
+import { Card, CardContent } from '../ui/card';
 import { Separator } from '../ui/Separator';
 import { HelpSupport } from './HelpSupport';
 import {
